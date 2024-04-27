6b7459172fadd5e82f1ff54051fdc9c5f66c2af1
vivekbargude <vivekbargude1206@gmail.com>
2024-04-27 20:52:00 +0530

    final commit

--- server.js
+++ server.js
@@ -4,11 +4,8 @@
 const authRouter = require('./routes/auth');
 const db = require('./config/db');
 const app = require('./app/app');
-
-app.use('/',doctorRouter);
 const PORT = 3000
 const http = require('http');
-const doctorRouter = require('./routes/doctor');
 const server =  http.createServer(app);
 server.listen(PORT, () => {
     console.log(`Socket Server listening on port ${PORT}`.yellow.bold);
