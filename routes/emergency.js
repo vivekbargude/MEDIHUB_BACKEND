b7102e7d39249e89e50a75429f0e2fad275eb09b
vivekbargude <vivekbargude1206@gmail.com>
2024-04-27 20:07:37 +0530

    final commit

--- routes/emergency.js
+++ routes/emergency.js
@@ -1,45 +1,45 @@
-// const express = require('express');
-// const auth = require('../middlewares/auth');
-// const emergencyRouter = express.Router();
-// require('dotenv').config();
-// const accountSid = process.env.accountSid;
-// const authToken = process.env.authToken;
-// const client = require('twilio')(accountSid, authToken);
-// const User = require('../model/usermodel');
+const express = require('express');
+const auth = require('../middlewares/auth');
+const emergencyRouter = express.Router();
+require('dotenv').config();
+const accountSid = process.env.accountSid;
+const authToken = process.env.authToken;
+const client = require('twilio')(accountSid, authToken);
+const User = require('../model/usermodel');
 
 
-// emergencyRouter.post('/api/send-message',auth,async(req,res)=>{
+emergencyRouter.post('/api/send-message',auth,async(req,res)=>{
 
-//     try {
-//         const { emergencyNumber , messageBody } = req.body;
-//         const user = await User.findById(req.user);
-//         const customMessage = `Emergency message from ${user.name}: ${messageBody}`; 
+    try {
+        const { emergencyNumber , messageBody } = req.body;
+        const user = await User.findById(req.user);
+        const customMessage = `Emergency message from ${user.name}: ${messageBody}`; 
 
-//         client.messages
-//                     .create({
-//                     body: customMessage,
-//                     from: '+13343104789',
-//                     to: emergencyNumber
-//                     })
-//     .then(message => {
-//       console.log(message.sid);
-//       res.send('Message sent successfully');
-//     })
-//     .catch(error => {
-//       console.error(error);
-//       res.status(500).send('Error sending message');
-//     });
+        client.messages
+                    .create({
+                    body: customMessage,
+                    from: '+13343104789',
+                    to: emergencyNumber
+                    })
+    .then(message => {
+      console.log(message.sid);
+      res.send('Message sent successfully');
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('Error sending message');
+    });
 
 
         
-//     } catch (error) {
+    } catch (error) {
 
-//         res.status(500).json({
-//             success : false,
-//             message: 'Server error'
-//         });
+        res.status(500).json({
+            success : false,
+            message: 'Server error'
+        });
         
-//     }
-// })
+    }
+})
 
-// module.exports = emergencyRouter;
+module.exports = emergencyRouter;
